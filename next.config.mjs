0ca0f3896b7023d74fcd25048472417ce556ea98
panfeng <319874759@qq.com>
2024-05-08 08:51:51 +0800

    build(next config): 修改nextjs 配置

--- next.config.mjs
+++ next.config.mjs
@@ -2,20 +2,21 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.TARGET}/:path*`,
-      },
-    ];
-  },
-  compiler: {},
-  trailingSlash: false,
-  images: {
-    deviceSizes: [390, 435, 768, 1024, 1280]
-  },
+	output: "standalone",
+	reactStrictMode: true,
+	async rewrites() {
+		return [
+			{
+				source: "/api/:path*",
+				destination: `${process.env.TARGET}/:path*`,
+			},
+		];
+	},
+	compiler: {},
+	trailingSlash: false,
+	images: {
+		deviceSizes: [390, 435, 768, 1024, 1280]
+	},
 };
 
 // const millionConfig = {
